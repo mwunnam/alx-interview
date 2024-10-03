d3aaf173faf735f751a3417b811aa18a6f591870
mwunnam <mwunnam@gmail.com>
2024-10-03 23:54:40 +0000

    correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,10 +7,10 @@ def makeChange(coins, total):
         return 0
 
     dp = [float('inf')] * (total + 1)
-    dp = [0] = 0
+    dp[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf' else -1)
+    return dp[total] if dp[total] != float('inf') else -1
