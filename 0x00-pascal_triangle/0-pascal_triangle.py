8d6808d88dcf72e037c311ef331757c30a30e73f
mwunnam <mwunnam@gmail.com>
2024-11-16 00:36:19 +0000

    done with task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([last_row[j] + last_row[j + 1] for j in range(
+                       len(last_row) - 1)])
+            row.append(1)
+        triangle.append(row)
+    return triangle
